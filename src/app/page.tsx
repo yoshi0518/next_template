6a765a4acb838ecda15086e3d0c4c9272bb17a4b
yoshi0518 <yoshihumi.n.0518@gmail.com>
2025-02-11 19:39:04 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import type { FC } from "react";
+
+const Home: FC = () => {
+  return (
+    <main>
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col items-center">
+          <h1 className="text-2xl font-bold">Hello World</h1>
+        </div>
+      </div>
+    </main>
+  );
+};
+export default Home;
