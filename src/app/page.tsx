a727da25a9485101efb2cd828572006ffbca6acb
yoshi0518 <yoshihumi.n.0518@gmail.com>
2025-02-13 09:40:31 +0900

    Prettierを設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import type { FC } from "react";
+import type { FC } from 'react';
 
 const Home: FC = () => {
   return (
     <main>
-      <div className="flex justify-center items-center h-screen">
+      <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col items-center">
           <h1 className="text-2xl font-bold">Hello World 2</h1>
         </div>
